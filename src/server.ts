d78593c2e78798e4896480cffba0646e476ed2bf
Christopher Willard <thatguychrisw@gmail.com>
2019-08-25 08:48:47 -0400

    Rewrite URLs /* -> /.netlify/functions/server/:splat - 8

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,10 @@ import express from 'express';
 import serverless from 'serverless-http';
 
 const app = express();
-const router = express.Router();
 
-router.all('/nf-test', (request, response) => response.json({
+app.all('/nf-test', (request, response) => response.json({
     text: '💙 Netlify Functions!',
 }));
-
-app.use('/.netlify/functions/server', router);
-app.use('/', router);
 
 export default app;
 export const handler = serverless(app);
