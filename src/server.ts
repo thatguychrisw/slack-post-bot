434b98f65799ed1c66810813d0653a891bba6c3d
Christopher Willard <thatguychrisw@gmail.com>
2019-08-24 22:13:47 -0400

    Remove potentially unneeded code?

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,11 @@ import express from 'express';
 import serverless from 'serverless-http';
 
 const app = express();
-const router = express.Router();
 
-router.get('/', (request, response) => {
+app.get('/', (request, response) => {
     response.send('Hello world!');
 });
 
-app.use('/', router);
 
 export default app;
 export const handler = serverless(app);
