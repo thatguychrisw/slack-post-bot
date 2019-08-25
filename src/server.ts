80fa17a9190cbba88781f57d0cd9fc3b7ce1eda1
Christopher Willard <thatguychrisw@gmail.com>
2019-08-24 23:56:51 -0400

    Rewrite URLS - Test 5

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import serverless from 'serverless-http';
+import bodyParser from 'body-parser';
 
 const app = express();
 const router = express.Router();
 
 router.all('/nf-test', (request, response) => {
-    response.send('💙 Netlify Functions!');
+    response.json({
+        'text': '💙 Netlify Functions!',
+    });
 });
 
+app.use(bodyParser);
 app.use('/.netlify/functions/server', router);
 
 export default app;
